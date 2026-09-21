eac7ef3a0f0066e4af073de66ce10f180296c2b4
trolly67 <pervoykinmaxim@gmail.com>
2026-09-21 12:25:52 +0700

    Lab 2: Add empty RegistrationActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.activity.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.appcompat)
